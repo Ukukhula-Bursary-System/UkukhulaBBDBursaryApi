e7c0e0adeef44153bf224eb221edfc455414136a
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-24 17:44:39 +0200

    Allowing Cross origin

--- src/main/java/com/bbd/bursary/manager/security/SecurityConfiguration.java
+++ src/main/java/com/bbd/bursary/manager/security/SecurityConfiguration.java
@@ -17,7 +17,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable)
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/authentication/login").permitAll()
                         .anyRequest().authenticated())
